c2fdb63ffb7d4c6628c62642478e6a0405434828
eliaszugon <elias.zuniga.bils@gmail.com>
2024-06-21 19:44:29 -0600

    first commit

---
+++ uploads/uag-plugin/assets/0/uag-js-2.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-dolph577' );
+});
+window.addEventListener("DOMContentLoaded", function(){
+	var swiper = new Swiper( ".uagb-block-7bda7b96 .uagb-swiper",
+		{"autoplay":{"delay":3000,"disableOnInteraction":false,"pauseOnMouseEnter":true,"stopOnLastSlide":false},"loop":true,"speed":800,"effect":"slide","direction":"horizontal","flipEffect":{"slideShadows":false},"fadeEffect":{"crossFade":true},"pagination":{"el":".uagb-block-7bda7b96 .swiper-pagination","clickable":true,"hideOnClick":false},"navigation":{"nextEl":".uagb-block-7bda7b96 .swiper-button-next","prevEl":".uagb-block-7bda7b96 .swiper-button-prev"}}	);
+});
+
+window.addEventListener("DOMContentLoaded", function(){
+	var swiper = new Swiper( ".uagb-block-ef1638ab .uagb-swiper",
+		{"autoplay":{"delay":3000,"disableOnInteraction":false,"pauseOnMouseEnter":true,"stopOnLastSlide":false},"loop":true,"speed":800,"effect":"slide","direction":"horizontal","flipEffect":{"slideShadows":false},"fadeEffect":{"crossFade":true},"pagination":{"el":".uagb-block-ef1638ab .swiper-pagination","clickable":true,"hideOnClick":false},"navigation":{"nextEl":".uagb-block-ef1638ab .swiper-button-next","prevEl":".uagb-block-ef1638ab .swiper-button-prev"}}	);
+});
+
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-d0100f3e' );
+});
+window.addEventListener("DOMContentLoaded", function(){
+	UAGBForms.init( {"block_id":"rzpqcnix","reCaptchaEnable":false,"reCaptchaType":"v2","reCaptchaSiteKeyV2":"","reCaptchaSecretKeyV2":"","reCaptchaSiteKeyV3":"","reCaptchaSecretKeyV3":"","afterSubmitToEmail":"abc@gmail.com","afterSubmitCcEmail":"","afterSubmitBccEmail":"","afterSubmitEmailSubject":"Form Submission","sendAfterSubmitEmail":true,"confirmationType":"message","hidereCaptchaBatch":false,"captchaMessage":"Please fill up the above captcha.","confirmationUrl":""}, '.uagb-block-rzpqcnix', 2 );
+});
+ });
---
+++ uploads/uag-plugin/assets/0/uag-js-295.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-ae4f01dc' );
+});
+ });
